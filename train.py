c6110f32d9f7835e821ff2154c40b41f1cf17e3a
Mindtraveller <vladimir.penyazkov@gmail.com>
2017-11-25 18:13:55 +0300

    Updated CNN

--- train.py
+++ train.py
@@ -5,8 +5,8 @@ from keras.layers import Dense, Conv2D, Flatten, Activation, MaxPooling2D, Dropo
 from keras.utils import plot_model
 from keras.callbacks import History, TerminateOnNaN, TensorBoard, CSVLogger
 
-IMAGE_WIDTH = 128
-IMAGE_HEIGHT = 128
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 300
 
 def train():
     model = Sequential()
@@ -26,9 +26,9 @@ def train():
     # model.add(Dropout(0.5))
 
     model.add(Flatten())
-    model.add(Dense(64))
+    model.add(Dense(2048))
     model.add(Activation('sigmoid'))
-    model.add(Dense(10))
+    model.add(Dense(57))
     model.add(Activation('sigmoid'))
 
     model.compile(
@@ -39,28 +39,28 @@ def train():
 
     model.fit_generator(
         ImageDataGenerator(rescale=1. / 255).flow_from_directory(
-            directory='data',
+            directory='images',
             target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-            batch_size=16
+            batch_size=20
         ),
-        validation_data=ImageDataGenerator(rescale=1. / 255).flow_from_directory(
-            directory='test',
-            target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-            batch_size=16
-        ),
-        validation_steps=2,
+        # validation_data=ImageDataGenerator(rescale=1. / 255).flow_from_directory(
+        #     directory='test',
+        #     target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+        #     batch_size=16
+        # ),
+        # validation_steps=2,
         epochs=20,
-        steps_per_epoch=190 // 16,
+        steps_per_epoch=57000 // 16,
         verbose=2,
         callbacks=[
             TerminateOnNaN(),
             # TensorBoard(log_dir='./logs', write_images=True),
-            CSVLogger('./logs/log.txt', append = False)
+            # CSVLogger('./logs/log.txt', append = False)
         ]
     )
 
     model.save('model.h5')
-    plot_model(model, 'model.png', show_shapes=True)
+    # plot_model(model, 'model.png', show_shapes=True)
 
 if __name__ == '__main__':
     train()
