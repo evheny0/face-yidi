e6a29af23223d9a1b3c29e56c40ff03d868aae0f
Eugene Haisenka <evheny0@gmail.com>
2017-11-25 19:57:41 +0300

    Fix image crop

--- feature.py
+++ feature.py
@@ -29,6 +29,8 @@ def crop_image(filename):
   image_data_bw = image_data.max(axis=2)
   non_empty_columns = np.where(image_data_bw.max(axis=0)>0)[0]
   non_empty_rows = np.where(image_data_bw.max(axis=1)>0)[0]
+  if (not non_empty_rows) or (not non_empty_columns):
+    return
   cropBox = (min(non_empty_rows), max(non_empty_rows), min(non_empty_columns), max(non_empty_columns))
 
   image_data_new = image_data[cropBox[0]:cropBox[1]+1, cropBox[2]:cropBox[3]+1 , :]
