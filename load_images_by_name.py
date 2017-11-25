98c5963e7b25d2b4e829ad6c09559f868dcc5bda
pavelsavchik <p.savchik@gmail.com>
2017-11-25 17:03:36 +0300

    Added script for images loading.

---
+++ load_images_by_name.py
@@ -0,0 +1,49 @@
+from urllib import request
+import json
+import os
+import _thread
+import sys
+
+IMAGES_PATH = "./images"
+
+API_URL = "https://www.googleapis.com/customsearch/v1"
+API_KEY = "AIzaSyCl1UzajNrpvya3dCGVvECTtMHiCKiGOIw"
+API_CX_KEY = "011644667007826062821:9i8kosv4i2o"
+API_SEARCH_TYPE = "image"
+API_IMAGE_TYPE = "face"
+API_BATCH_SIZE = 10
+API_IMAGE_SIZE = "medium"
+BASIC_SEARCH_URL = f"{API_URL}?key={API_KEY}&cx={API_CX_KEY}&searchType={API_SEARCH_TYPE}&imageType={API_IMAGE_TYPE}&num={API_BATCH_SIZE}&imageSize={API_IMAGE_SIZE}"
+
+TOTAL_IMAGES = 100
+
+NAMES = sys.argv[1].split(" ")
+
+def loadImages(name):
+	nameFolderPath = initFolder(name)
+	offset = 1
+	threads = []
+	while offset < TOTAL_IMAGES:
+		loadImagesBatch(name, nameFolderPath, offset)
+		offset = offset + API_BATCH_SIZE
+
+def loadImagesBatch(name, folder, offset):
+	encodedName = request.quote(name.encode('cp1251'))
+	url = f"{BASIC_SEARCH_URL}&q={encodedName}&start={offset}"
+	print(url)
+	response = json.loads(request.urlopen(url).read())
+	for index, item in enumerate(response["items"]):
+		filename = f"{folder}/{index + offset}.jpg"
+		print(filename, item['link'])
+		request.urlretrieve(item['link'], filename)
+
+def initFolder(name):
+	nameFolderPath = f"{IMAGES_PATH}/{name}" 
+	if not os.path.exists(nameFolderPath):
+		os.makedirs(nameFolderPath)
+	return nameFolderPath;
+
+for name in NAMES:
+	print(f"Images loading started for name {name}")	
+	loadImages(name)
+	print(f"Images loading finished for name {name}")
