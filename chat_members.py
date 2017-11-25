5f84acb3b00e337a734e4f5af133635e18220967
Eugene Haisenka <evheny0@gmail.com>
2017-11-25 15:49:33 +0300

    Telek parser

--- chat_members.py
+++ chat_members.py
@@ -1,6 +1,11 @@
 from telethon import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty
+from telethon.tl.functions.channels import GetParticipantsRequest
+from telethon.tl.types import ChannelParticipantsSearch
+from telethon.tl.types import InputChannel
+
+
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
@@ -8,14 +13,23 @@ api_id = 123
 api_hash = '123'
 phone = '+37529***6387'
 
-client = TelegramClient('session_name', api_id, api_hash)
-client.connect()
+# client = TelegramClient('session_name', api_id, api_hash)
+# client.connect()
 
 # If you already have a previous 'session_name.session' file, skip this.
-client.sign_in(phone=phone)
-code = str(input("Confirmation code: "))
-me = client.sign_in(code=code)
-print(me.stringify())
+# client.sign_in(phone=phone)
+# code = str(input("Confirmation code: "))
+# me = client.sign_in(code=code)
+# print(me.stringify())
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    client.sign_in(phone=phone)
+    code = str(input("Confirmation code: "))
+    me = client.sign_in(code=code)
+    # client.send_code_request('+15xxxxxxxxx')
+    # client.sign_in('+15xxxxxxxxx', cod)
 
 dialogs = []
 users = []
@@ -34,6 +48,22 @@ users.extend(result.users)
 chats.extend(result.chats)
 
 print(dialogs)
+
+
+
+# InputChannel(dialogs[0].id, dialogs[0].access_hash)
+
+participants = client.invoke(GetParticipantsRequest(
+    InputChannel(chats[0].id, chats[0].access_hash), ChannelParticipantsSearch(''), 0, 300, 1
+))
+
+# print(participants.users[0].stringify())
+
+with open("names2.txt", "w") as output:
+    for user in participants.users:
+        output.write(user.first_name)
+        output.write("\n")
+
 
 with open("dialogs.txt", "w") as output:
     output.write(str(dialogs))
