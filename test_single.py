03334a79ba624302962eca4d028915b3c74f7f03
Mindtraveller <vladimir.penyazkov@gmail.com>
2017-11-26 15:10:26 +0300

    Updated server

--- test_single.py
+++ test_single.py
@@ -6,14 +6,14 @@ model = models.load_model('model.h5')
 
 def testImage(imagePath):
 	print(f"Load image from {imagePath}")
-    image = load_img(imagePath)
-    print("Loaded")
-    imageArray = img_to_array(image)
-    print("Converted to array")
-    predictions = model.predict(imageArray)
-    return predictions
+	image = load_img(imagePath)
+	print("Loaded")
+	imageArray = img_to_array(image)
+	print("Converted to array")
+	predictions = model.predict(imageArray)
+	return predictions
 
 if __name__ == '__main__':
-    test()
+	test()
 
 
