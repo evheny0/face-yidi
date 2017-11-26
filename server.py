fbb2fee10cbd666affc51331659dc15d38902dc1
Mindtraveller <vladimir.penyazkov@gmail.com>
2017-11-26 14:05:46 +0300

    Updated server

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import request
 import uuid
+import base64
 import test_single
 
 app = Flask("hashleta")
 
 @app.route("/image", methods=['POST'])
 def processImage():
+	print('request received')
 	image = request.files['image']
 	filePath = f'./uploaded/{str(uuid.uuid4())}.jpg'
 	image.save(filePath)
@@ -14,4 +16,4 @@ def processImage():
 	return predictions
 
 if __name__ == "__main__":
-	app.run(port=8080)
+	app.run(host='0.0.0.0', port=8080)
