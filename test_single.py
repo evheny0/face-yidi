324ad0a8034d9ef6ebaf94f14913a5b373c969fd
Mindtraveller <vladimir.penyazkov@gmail.com>
2017-11-26 15:09:32 +0300

    Updated server

--- test_single.py
+++ test_single.py
@@ -5,8 +5,11 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 model = models.load_model('model.h5')
 
 def testImage(imagePath):
+	print(f"Load image from {imagePath}")
     image = load_img(imagePath)
+    print("Loaded")
     imageArray = img_to_array(image)
+    print("Converted to array")
     predictions = model.predict(imageArray)
     return predictions
 
